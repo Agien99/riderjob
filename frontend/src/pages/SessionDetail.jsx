489b44a8a09d64a1063653e9d34c9745beec2e4f
Agien99 <hypertyzer@gmail.com>
2026-09-09 09:42:12 +0800

    Rename SessionDetails.jsx to SessionDetail.jsx

---
+++ frontend/src/pages/SessionDetail.jsx
@@ -0,0 +1,9 @@
+function SessionDetail() {
+  return (
+    <main>
+      <h1>Session Detail</h1>
+    </main>
+  )
+}
+
+export default SessionDetail
--- frontend/src/pages/SessionDetails.jsx
+++
@@ -1,9 +0,0 @@
-function SessionDetails() {
-  return (
-    <main>
-      <h1>Session Details</h1>
-    </main>
-  )
-}
-
-export default SessionDetails
